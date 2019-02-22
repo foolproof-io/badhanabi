acc2e579575e1547c9748149a56cc4b7a84c96c1
Ryan P. Brewster <RyanPBrewster@gmail.com>
2019-02-21 23:04:54 -0800

    install and run prettier

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,19 @@
 import * as _ from "lodash";
 import m from "mithril";
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 function strictParse(value: string): number {
-  return /^(-|\+)?(\d+|Infinity)$/.test(value)
-    ? Number(value)
-    : NaN;
+  return /^(-|\+)?(\d+|Infinity)$/.test(value) ? Number(value) : NaN;
 }
 
 type Color = string;
 type Rank = string;
 type Tile = string;
 type Hint = string;
-const COLORS: Hint[] = ['R', 'G', 'B', 'Y', 'W', 'P'];
-const RANKS: Color[] = ['1', '2', '3', '4', '5'];
+const COLORS: Hint[] = ["R", "G", "B", "Y", "W", "P"];
+const RANKS: Color[] = ["1", "2", "3", "4", "5"];
 const UNKNOWN: Tile = "UU";
 
 interface HandItem {
@@ -45,17 +42,22 @@ function generateDeck(): Tile[] {
 }
 function handSize(num_players) {
   switch (num_players) {
-    case 2: return 6;
-    case 3: return 5;
-    case 4: return 4;
-    case 5: return 4;
-    default: return -1;
+    case 2:
+      return 6;
+    case 3:
+      return 5;
+    case 4:
+      return 4;
+    case 5:
+      return 4;
+    default:
+      return -1;
   }
 }
 const NUM_INITIAL_HINTS = 8;
 function drawTiles(tiles: Tile[], num_tiles: number): HandItem[] {
   let ts: HandItem[] = [];
-  for (let i=0; i < num_tiles; i++) {
+  for (let i = 0; i < num_tiles; i++) {
     ts.push({ tile: tiles.pop(), hints: [] });
   }
   return ts;
@@ -96,7 +98,7 @@ function applyHintToHand(hand, hint) {
 }
 
 function removeCardFromHand(hand, idx) {
-  let minus_card = hand.slice(0, idx).concat(hand.slice(idx+1));
+  let minus_card = hand.slice(0, idx).concat(hand.slice(idx + 1));
   // trim off any leading hints, they give no information
   while (minus_card.length > 0 && minus_card[0].hint) {
     minus_card.shift();
@@ -110,78 +112,100 @@ function matchesHint(tile, hint) {
 
 const ROOM_STATES = {
   WAITING_TO_START: "waiting to start",
-  WAITING_FOR_PLAYER: player => `waiting for ${player}`,
+  WAITING_FOR_PLAYER: player => `waiting for ${player}`
 };
 function viewModel(model, handler) {
   return m("div", [
-    m('div', { id: "whoami", }, `You are: ${model.uid}`),
-    m('div', { id: "state", }, model.room.state || "waiting to start"),
-    m('div', { id: "draws", }, `Draws: ${(model.room.draw_pile || []).length}`),
-    m('div', { id: "hints", }, `Hints: ${model.room.hints}`),
-    m('div', { id: "errors", }, `Errors: ${model.room.errors}`),
-    m('div', { id: "discards", }, `Discards: ${model.room.discard_pile || []}`),
-    m('div', { id: "plays", }, [ "Plays:", viewPlayPile(model.room.play_pile || []) ]),
-
-    m('div', {
-      id: "players",
-    }, [
-      "Players:",
-      rotateToLast(model.room.players || model.room.viewers || [], model.uid).map(player => {
-        const name = (model.room.names && model.room.names[player]) || player;
-        if (!model.room.hands) {
-          return m('p', name);
-        }
-        const hand = model.room.hands[player];
-        const player_view = viewPlayer(name, player === model.uid ? redactTiles(hand) : hand);
-        return m('div', {
-          class: model.room.state === ROOM_STATES.WAITING_FOR_PLAYER(player) ? "current_player" : "waiting_player",
-        }, player_view);
-      })
+    m("div", { id: "whoami" }, `You are: ${model.uid}`),
+    m("div", { id: "state" }, model.room.state || "waiting to start"),
+    m("div", { id: "draws" }, `Draws: ${(model.room.draw_pile || []).length}`),
+    m("div", { id: "hints" }, `Hints: ${model.room.hints}`),
+    m("div", { id: "errors" }, `Errors: ${model.room.errors}`),
+    m("div", { id: "discards" }, `Discards: ${model.room.discard_pile || []}`),
+    m("div", { id: "plays" }, [
+      "Plays:",
+      viewPlayPile(model.room.play_pile || [])
     ]),
 
-    m('input', {
+    m(
+      "div",
+      {
+        id: "players"
+      },
+      [
+        "Players:",
+        rotateToLast(
+          model.room.players || model.room.viewers || [],
+          model.uid
+        ).map(player => {
+          const name = (model.room.names && model.room.names[player]) || player;
+          if (!model.room.hands) {
+            return m("p", name);
+          }
+          const hand = model.room.hands[player];
+          const player_view = viewPlayer(
+            name,
+            player === model.uid ? redactTiles(hand) : hand
+          );
+          return m(
+            "div",
+            {
+              class:
+                model.room.state === ROOM_STATES.WAITING_FOR_PLAYER(player)
+                  ? "current_player"
+                  : "waiting_player"
+            },
+            player_view
+          );
+        })
+      ]
+    ),
+
+    m("input", {
       id: "user_input",
       onkeypress: handler,
       placeholder: "user_input goes here",
-      autofocus: true,
+      autofocus: true
     }),
 
-    m('div', { id: "helptext" }, model.helptext),
+    m("div", { id: "helptext" }, model.helptext),
 
-    m('div', {
-      id: "actions",
-    }, model.actions.map(msg => m('p', msg.text))),
+    m(
+      "div",
+      {
+        id: "actions"
+      },
+      model.actions.map(msg => m("p", msg.text))
+    )
   ]);
 }
 function viewPlayPile(play_pile) {
   const summary = summarizePlayPile(play_pile);
-  return m('table', [
-    m('tr', COLORS.map(c => m('td', viewPile(c, summary[c])))),
+  return m("table", [
+    m("tr", COLORS.map(c => m("td", viewPile(c, summary[c]))))
   ]);
 }
 function viewPile(color, rank) {
-    return m('img', {
-      class: "pile",
-      src: `./imgs/piles/${color}${rank}.svg`,
-    });
+  return m("img", {
+    class: "pile",
+    src: `./imgs/piles/${color}${rank}.svg`
+  });
 }
 function viewPlayer(player_name, hand) {
-  return m('div', [
+  return m("div", [
     player_name,
-    m('table', [
-      m('tr', hand.map((item, idx) => m('td', idx))),
-      m('tr', hand.map(item => m('td', viewHandItem(item)))),
-      m('tr', hand.map(item => m('td', viewTileHints(item.hints)))),
-    ]),
+    m("table", [
+      m("tr", hand.map((item, idx) => m("td", idx))),
+      m("tr", hand.map(item => m("td", viewHandItem(item)))),
+      m("tr", hand.map(item => m("td", viewTileHints(item.hints))))
+    ])
   ]);
 }
 function viewHandItem(item) {
-  return item.tile
-    ? viewTile(item.tile)
-    : viewMarker(item.hint);
+  return item.tile ? viewTile(item.tile) : viewMarker(item.hint);
 }
 function viewTile(tile) {
-  return m('img', {
+  return m("img", {
     class: "tile",
     src: `./imgs/tiles/${tile}.svg`
   });
@@ -192,14 +216,14 @@ function viewTileHints(hints: Hint[]): m.Child {
   }
   const color = _.find(hints, h => COLORS.indexOf(h) >= 0);
   const rank = _.find(hints, h => RANKS.indexOf(h) >= 0);
-  return m('div', [
-    m('img', { class: "hint", src: `./imgs/hints/${color || "U"}.svg` }),
-    m('img', { class: "hint", src: `./imgs/hints/${rank || "U"}.svg` }),
+  return m("div", [
+    m("img", { class: "hint", src: `./imgs/hints/${color || "U"}.svg` }),
+    m("img", { class: "hint", src: `./imgs/hints/${rank || "U"}.svg` })
   ]);
 }
 
 function viewMarker(hint: Hint): m.Child {
-  return m('img', {
+  return m("img", {
     class: "marker",
     src: `./imgs/hints/${hint}.svg`
   });
@@ -207,20 +231,18 @@ function viewMarker(hint: Hint): m.Child {
 
 function rotateToLast(xs, x) {
   const idx = xs.indexOf(x);
-  return idx === -1
-    ? xs
-    : xs.slice(idx + 1).concat(xs.slice(0, idx + 1));
+  return idx === -1 ? xs : xs.slice(idx + 1).concat(xs.slice(0, idx + 1));
 }
 function redactTiles(hand) {
-  return hand.map(item => item.tile ? redactTile(item) : item);
+  return hand.map(item => (item.tile ? redactTile(item) : item));
 }
 function redactTile(item) {
   return { tile: UNKNOWN, hints: item.hints };
 }
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
   firebase.initializeApp({
     apiKey: "AIzaSyBIb0BX7QA5K42j12QZH_E3UB8QH_sPpr8",
-    projectId: "foolproof-hanabi",
+    projectId: "foolproof-hanabi"
   });
   const room_id = window.location.pathname.substring(1);
   let model: any = {
@@ -236,23 +258,33 @@ document.addEventListener('DOMContentLoaded', function() {
       hints: null,
       discard_pile: null,
       play_pile: null,
-      draw_pile: null,
+      draw_pile: null
     },
     uid: null,
-    view: () => viewModel(model, handler),
+    view: () => viewModel(model, handler)
   };
 
   const app = firebase.app();
   const remote = {
-    room: app.firestore().collection("rooms").doc(room_id),
-    actions: app.firestore().collection("rooms").doc(room_id).collection("actions"),
+    room: app
+      .firestore()
+      .collection("rooms")
+      .doc(room_id),
+    actions: app
+      .firestore()
+      .collection("rooms")
+      .doc(room_id)
+      .collection("actions")
   };
   app.auth().onAuthStateChanged(evt => {
     if (evt && evt.uid) {
       model.uid = evt.uid;
-      remote.room.set({
-        viewers: firebase.firestore.FieldValue.arrayUnion(model.uid),
-      }, {merge:true});
+      remote.room.set(
+        {
+          viewers: firebase.firestore.FieldValue.arrayUnion(model.uid)
+        },
+        { merge: true }
+      );
     }
   });
   remote.room.onSnapshot(snap => {
@@ -273,7 +305,7 @@ document.addEventListener('DOMContentLoaded', function() {
   function log(msg) {
     remote.actions.add({
       time: firebase.firestore.FieldValue.serverTimestamp(),
-      text: msg,
+      text: msg
     });
     return true;
   }
@@ -288,7 +320,9 @@ document.addEventListener('DOMContentLoaded', function() {
     if (!model.room.state) {
       return note("this game hasn't started yet, try 'start'");
     }
-    return note("you can 'discard <tile_idx>', 'play <tile_idx>', or 'hint <player> <hint>'");
+    return note(
+      "you can 'discard <tile_idx>', 'play <tile_idx>', or 'hint <player> <hint>'"
+    );
   }
   function setName(name: string) {
     if (!model.uid || !model.room) {
@@ -310,7 +344,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const players = model.room.viewers;
     const hand_size = handSize(players.length);
     if (hand_size < 0) {
-      return note(`wrong number of players, we can support 2--5, you have ${players.length}`);
+      return note(
+        `wrong number of players, we can support 2--5, you have ${
+          players.length
+        }`
+      );
     }
     let deck = generateDeck();
     let hands = {};
@@ -318,14 +356,14 @@ document.addEventListener('DOMContentLoaded', function() {
       hands[player] = drawTiles(deck, hand_size);
     });
     remote.room.update({
-      players, 
+      players,
       draw_pile: deck,
       discard_pile: [],
       play_pile: [],
       hints: NUM_INITIAL_HINTS,
       errors: 0,
       hands,
-      state: ROOM_STATES.WAITING_FOR_PLAYER(players[0]),
+      state: ROOM_STATES.WAITING_FOR_PLAYER(players[0])
     });
     return log("game has begun!");
   }
@@ -339,7 +377,9 @@ document.addEventListener('DOMContentLoaded', function() {
       return note(`can't discard that, try again`);
     }
     let update = {};
-    update["state"] = ROOM_STATES.WAITING_FOR_PLAYER(playerAfter(model.room.players, model.uid));
+    update["state"] = ROOM_STATES.WAITING_FOR_PLAYER(
+      playerAfter(model.room.players, model.uid)
+    );
     update["hints"] = Math.max(model.room.hints + 1, NUM_INITIAL_HINTS);
     update["discard_pile"] = model.room.discard_pile.concat(hand[idx].tile);
     const next_tile = model.room.draw_pile[0];
@@ -362,7 +402,9 @@ document.addEventListener('DOMContentLoaded', function() {
       return note(`can't play that, try again`);
     }
     let update = {};
-    update["state"] = ROOM_STATES.WAITING_FOR_PLAYER(playerAfter(model.room.players, model.uid));
+    update["state"] = ROOM_STATES.WAITING_FOR_PLAYER(
+      playerAfter(model.room.players, model.uid)
+    );
     if (isLegalPlay(model.room.play_pile, hand[idx].tile)) {
       update["play_pile"] = model.room.play_pile.concat(hand[idx].tile);
       log(`${model.uid} played ${hand[idx].tile}`);
@@ -379,7 +421,7 @@ document.addEventListener('DOMContentLoaded', function() {
       update["draw_pile"] = model.room.draw_pile.slice(1);
       update[`hands.${model.uid}`] = removeCardFromHand(hand, idx).concat({
         tile: next_tile,
-        hints: [],
+        hints: []
       });
     }
     remote.room.update(update);
@@ -391,13 +433,16 @@ document.addEventListener('DOMContentLoaded', function() {
     if (model.room.hints === 0) {
       return note("no hints left");
     }
-    const matching_players: string[] =
-      _.flatMap(model.room.players, (player: string) => {
+    const matching_players: string[] = _.flatMap(
+      model.room.players,
+      (player: string) => {
         const name = (model.room.names && model.room.names[player]) || player;
-        return player !== model.uid && name.toLowerCase().startsWith(target_prefix.toLowerCase())
-          ? [ player ]
+        return player !== model.uid &&
+          name.toLowerCase().startsWith(target_prefix.toLowerCase())
+          ? [player]
           : [];
-      });
+      }
+    );
     if (matching_players.length === 0) {
       return note("prefix doesn't match any of the others players");
     }
@@ -409,24 +454,32 @@ document.addEventListener('DOMContentLoaded', function() {
       return note("no such player");
     }
     const h = hint.toUpperCase(); // canonicalize to uppercase
-    if (h.length !== 1 || COLORS.indexOf(h) === -1 && RANKS.indexOf(h) === -1) {
+    if (
+      h.length !== 1 ||
+      (COLORS.indexOf(h) === -1 && RANKS.indexOf(h) === -1)
+    ) {
       return note("invalid hint");
     }
     let update = {};
-    update["state"] = ROOM_STATES.WAITING_FOR_PLAYER(playerAfter(model.room.players, model.uid));
+    update["state"] = ROOM_STATES.WAITING_FOR_PLAYER(
+      playerAfter(model.room.players, model.uid)
+    );
     update["hints"] = model.room.hints - 1;
-    update[`hands.${target_player}`] = applyHintToHand(model.room.hands[target_player], h);
+    update[`hands.${target_player}`] = applyHintToHand(
+      model.room.hands[target_player],
+      h
+    );
     remote.room.update(update);
     return log(`${model.uid} told ${target_player} about ${hint}`);
   }
   const commands = {
-    "help": help,
+    help: help,
     "?": help,
-    "name": setName,
-    "start": startGame,
-    "hint": giveHint,
-    "discard": discardTile,
-    "play": playTile,
+    name: setName,
+    start: startGame,
+    hint: giveHint,
+    discard: discardTile,
+    play: playTile
   };
   function perform(action) {
     if (!model.uid) {
@@ -435,7 +488,10 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     for (let cmd in commands) {
       if (action.startsWith(cmd)) {
-        const args = action.substring(cmd.length).trim().split(/\s+/);
+        const args = action
+          .substring(cmd.length)
+          .trim()
+          .split(/\s+/);
         commands[cmd].apply(null, args);
         return true;
       }
